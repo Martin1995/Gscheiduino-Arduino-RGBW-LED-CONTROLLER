85f9505365d6e3b112cbebc9bb16a6088e269a36
Martin1995 <Martin1995@users.noreply.github.com>
2017-09-06 21:28:11 +0200

    Updated versiion with Central Off/ON Button

--- RGBW-LIGHT-CONTROLLER/RGBW-LIGHT-CONTROLLER.ino
+++ RGBW-LIGHT-CONTROLLER/RGBW-LIGHT-CONTROLLER.ino
@@ -9,14 +9,17 @@
   #define TWO 4
   #define THREE 5
   #define FOUR 7
+  #define FIVE 8
   int brightness1 = 0; //Starthelligkeit LED1
   int brightness2 = 255; //Starthelligkeit LED2
   int brightness3 = 125; //Starthelligkeit LED3
   int fadeAmount1 = 5; //Zählergröße LED1
   int fadeAmount2 = fadeAmount1; //Zählergröße LED2
   int fadeAmount3 = fadeAmount2; //Zählergröße LED3
-  int blink = 0; //Ausganszustand Status_LED
-  int fader = 1; //FADER == OFF
+  int ledpuls = 1; //Ausganszustand Status_LED
+  int central_off = 1; //Zentral-Aus-EIN ==> ON
+  int fader = 1; //FADER == ON
+
 
 
   
@@ -32,33 +35,59 @@ void setup() {
   pinMode (TWO, INPUT); //TWO-Button Eingang
   pinMode (THREE, INPUT); //THREE-Button Eingang
   pinMode (FOUR, INPUT); //FOUR-BUTTON Eingang
-  digitalWrite (FADER, HIGH); //FADER-Button Eingang
-  digitalWrite (ONE, HIGH); //ONE-Button Eingang
-  digitalWrite (TWO, HIGH); //TWO-Button Eingang
-  digitalWrite (THREE, HIGH); //THREE-Button Eingang
-  digitalWrite (FOUR, HIGH); //FOUR-BUTTON Eingang
+  pinMode (FIVE, INPUT); //FIVE-BUTTON Eingang
+  digitalWrite (FADER, HIGH); //FADER-Button Eingang pullup
+  digitalWrite (ONE, HIGH); //ONE-Button Eingang pullup
+  digitalWrite (TWO, HIGH); //TWO-Button Eingang pullup
+  digitalWrite (THREE, HIGH); //THREE-Button Eingang pullup
+  digitalWrite (FOUR, HIGH); //FOUR-BUTTON Eingang pullup
+  digitalWrite (FIVE, HIGH); //FIVE-BUTTON Eingang pullup
   digitalWrite (LED_4, HIGH); //LED4 Ausgang
   Serial.begin(9600);
 }
 
 void loop() {
+
   // put your main code here, to run repeatedly:
 
-  Serial.println("BUTTON1: " + String(digitalRead(ONE)) + ", BUTTON2: " + String(digitalRead(TWO)) + ", BUTTON3: " + String(digitalRead(THREE)) + ", BUTTON4: " + String(digitalRead(FOUR)) + ", FADER-BUTTON: " + String(digitalRead(FADER)) + ", FADER-STATE: " + String(fader));
+  Serial.println("BUTTON1: " + String(digitalRead(ONE)) + ", BUTTON2: " + String(digitalRead(TWO)) + ", BUTTON3: " + String(digitalRead(THREE)) + ", BUTTON4: " + String(digitalRead(FOUR)) + ", FADER-BUTTON: " + String(digitalRead(FADER)) + ", FADER-STATE: " + String(fader) + " BUTTON5: " + String(digitalRead(FIVE)) + " ON/OFF: " + String(central_off));
+    //Umschaltung der Status_LED mit Boolscher Algebra jeden Programm durchlauf
+
+    if (digitalRead(FIVE) == LOW && central_off == 0){
+      central_off = 1;
+    }
+    else if (digitalRead(FIVE) == LOW && central_off == 1) {
+      central_off = 0;
+    }
+    else {
+    }
+
+    if (central_off == 1) {
+    if (ledpuls == 0) {
+    ledpuls = ledpuls + 1;
+    }
+    else {
+    ledpuls = ledpuls - 1;
+    }
+    digitalWrite(LED_puls, ledpuls); //Wir schreiben den Ausganspegel HIGH/LOW für die Status_LED
+
+
   if (digitalRead(FADER) == LOW && fader == 0){
     digitalWrite(LED_1, LOW);
     digitalWrite(LED_2, LOW);
     digitalWrite(LED_3, LOW);
     fader = fader + 1;
+ 
   }
   else if (digitalRead(FADER) == LOW && fader == 1) {
     fader = fader - 1;
     digitalWrite(LED_1, LOW);
     digitalWrite(LED_2, LOW);
     digitalWrite(LED_3, LOW);
-    
+ 
   }
   else {
+ 
   }
 
   if (digitalRead(ONE) == LOW && (digitalRead(LED_1) == LOW || fader == 1) ){
@@ -72,11 +101,14 @@ void loop() {
      else {
       digitalWrite(LED_1, HIGH);
      }
+  
   }
   else if (digitalRead(ONE) == LOW && digitalRead(LED_1) == HIGH) {
     digitalWrite(LED_1, LOW);
+ 
     }
   else {
+ 
   }
   
   if (digitalRead(TWO) == LOW && (digitalRead(LED_2) == LOW || fader == 1 )){
@@ -90,11 +122,14 @@ void loop() {
      else{
       digitalWrite(LED_2, HIGH);
      }
+  
   }
   else if (digitalRead(TWO) == LOW && digitalRead(LED_2) == HIGH) {
     digitalWrite(LED_2, LOW);
+ 
     }
   else {
+ 
   }
   
   if (digitalRead(THREE) == LOW && (digitalRead(LED_3) == LOW || fader == 1 )){
@@ -108,11 +143,14 @@ void loop() {
      else{
       digitalWrite(LED_3, HIGH);
      }
+  
   }
   else if (digitalRead(THREE) == LOW && digitalRead(LED_3) == HIGH) {
     digitalWrite(LED_3, LOW);
+ 
     }
   else {
+ 
   }
   
   if (digitalRead(FOUR) == LOW && digitalRead(LED_4) == LOW  ){
@@ -130,7 +168,6 @@ void loop() {
   analogWrite(LED_1, brightness1); //wir schreiben den helligkeitswert für LED1
   analogWrite(LED_2, brightness2); //wir schreiben den helligkeitswert für LED2
   analogWrite(LED_3, brightness3); //wir schreiben den helligkeitswert für LED3
-  digitalWrite(LED_puls, blink); //Wir schreiben den Ausganspegel HIGH/LOW für die Status_LED
 
   // change the brightness for next time through the loop:
   //Wir ändern die helligkeit für den nächsten schleifen durchlauf:
@@ -149,18 +186,25 @@ void loop() {
   if (brightness3 <= 0 || brightness3 >= 255) {
     fadeAmount3 = -fadeAmount3;
   }
-  //Umschaltung der Status_LED mit Boolscher Algebra jeden Programm durchlauf
-  if (blink == 0) {
-    blink = blink + 1;
-  }
   else {
-    blink = blink - 1;
   }
+  }
+
   // wait for ... milliseconds to see the dimming effect
   //Wir warten ... Millisekunden um den Dimmereffekt zu sehen
   delay(75);
   }
+  if (central_off == 0) {
+    if (ledpuls == 0) {
+    ledpuls = ledpuls + 1;
+    }
+    else {
+    ledpuls = ledpuls - 1;
+    }
+    digitalWrite(LED_puls, ledpuls); //Wir schreiben den Ausganspegel HIGH/LOW für die Status_LED
+    delay(200);
 
-
-
+  }
 }
+
+
